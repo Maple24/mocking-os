33b0a17e96b8f3e626f581ce1c584db2b49d37a6
snake <ylvis1024@gmail.com>
2021-02-24 11:59:37 +0800

    add Stop process

--- model/Process.go
+++ model/Process.go
@@ -11,8 +11,14 @@ const (
 type Process struct {
 	Pid    uint32
 	Status State
+	Time   uint32
 }
 
 func (this *Process) ChangStatus(Status State) {
 	this.Status = Status
+}
+
+func (this *Process) StopProcess() {
+	this.Status = Blocked
+	this.Time = 0
 }
